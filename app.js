6d03c4f13053d59ba06c9d3db9ce74709c25000e
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2023-04-06 18:33:01 +0000

    simple express server

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import express from 'express'
 const PORT = 5000;
 const app = express()
 
-app.get('/',(eq,res)=>{
-    res.status(200).send('serve')
+app.get('/',(req,res)=>{
+    res.status(200).send('server')
 })
 
 app.listen(PORT,()=>{
