8bd4a35be069d8203264c7b7549a1501771ca796
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2023-04-07 10:56:55 +0000

    Delete Methods

--- Delete/drop.js
+++ Delete/drop.js
@@ -1,9 +1,13 @@
 //Drop is used for droping or deleting collections, 
 //if collection gets deleted automatically DB is also gets deleted because DN's without collections are not created
+import mongoose from "mongoose";
+ import "../dbConnect.js";
+import myModel from "../models/index.js";
+import NewCollection from "../Create/createCollection.js"
 
 async function dropCollection() {
     try {
-      let check = await myModel.collection.drop();
+      let check = await myModel.NewCollection.drop();
       console.log(check);
       mongoose.connection.close();
     } catch (error) {
